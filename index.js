46b4ee0195b68e1d2b3a2696a170798c79e63a69
Austin <austinodhiambo64@gmail.com>
2023-03-14 23:24:37 +0300

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+
+const divide = (parameter1, parameter2) => {return 2000 / 100};
+
+
+const add = (parameter1, parameter2) => {return parameter1 + parameter2};
+
+const square = (parameter1) => {return parameter1 * parameter1};
